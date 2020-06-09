7a22bd9face89a1ec950353b508a96d6c785336b
eleblebici <ebrar.leblebici@hotmail.com>
2020-06-09 14:08:26 +0300

    Add files via upload

---
+++ annotater.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo -n "On which namespace you will annotate pods? : "
+read namespace
+IFS= read -r -d '' -a pods < <( kubectl get pods -n $namespace | awk 'NR > 1 {print $1}' && printf '\0' )
+declare -a podswhichhavePVC=()
+for pod in $pods
+do
+	if [[ $(kubectl describe pod $pod -n $namespace) == *"PersistentVolumeClaim"* ]]
+		then podswhichhavePVC+=($pod)
+		continue
+	fi
+done
+x=$(echo ${#podswhichhavePVC[@]})
+for ((i=0;i<x;i++))
+do
+	IFS= read -r -d '' -a volumenamesforpod < <( kubectl describe pod ${podswhichhavePVC[$i]} -n $namespace | grep -B1 'PersistentVolumeClaim' |  grep -v 'PersistentVolumeClaim' | grep -v "\-\-" | sed 's/://' | sed -e 's/[ ]*//' )
+	y=$(echo ${#volumenamesforpod[@]})
+	for ((a=0;a<y;a++))
+	do
+		kubectl annotate pods ${podswhichhavePVC[$i]} -n $namespace backup.velero.io/backup-volumes="${volumenamesforpod[$a]}" --overwrite
+#		echo pod ${podswhichhavePVC[i]} annotated with backup.velero.io/backup-volumes="${volumenamesforpod[a]}"
+	done
+done
+
